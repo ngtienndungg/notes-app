f1543522d2274cbc04d3341eab587d0550fe12c3
ngtienndungg <dungnt.works@gmail.com>
2023-06-07 20:13:15 +0700

    create note entity

--- app/src/main/java/com/example/notes/entities/Note.java
+++ app/src/main/java/com/example/notes/entities/Note.java
@@ -1,2 +1,106 @@
-package com.example.notes.entities;public class Note {
+package com.example.notes.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "notes")
+public class Note implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "date_time")
+    private String dateTime;
+
+    @ColumnInfo(name = "subtitle")
+    private String subtitle;
+
+    @ColumnInfo(name = "note_content")
+    private String noteContent;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    @ColumnInfo(name = "color")
+    private String color;
+
+    @ColumnInfo(name = "web_link")
+    private String webLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title + ": " + dateTime;
+    }
 }
