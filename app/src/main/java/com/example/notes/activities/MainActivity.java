43c5136c850ed4ead6cfffd1be66fff78fb08ca0
ngtienndungg <dungnt.works@gmail.com>
2023-06-08 15:18:44 +0700

    handle get all notes feature

--- app/src/main/java/com/example/notes/activities/MainActivity.java
+++ app/src/main/java/com/example/notes/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.notes.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.notes.R;
+import com.example.notes.database.NoteDatabase;
+import com.example.notes.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,19 +26,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Map view from layout
         viewMapping();
-        ivAddNoteMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(
-                        new Intent(getApplicationContext(), CreateNoteActivity.class),
-                        REQUEST_CODE_ADD_NOTE
-                );
-            }
-        });
+
+        // Handle set text event
+        onClickHandling();
+
+        // Handle click event
+        getNotes();
     }
 
     private void viewMapping() {
         ivAddNoteMain = findViewById(R.id.activity_main_ivAddNoteMain);
+    }
+
+    private void onClickHandling() {
+        ivAddNoteMain.setOnClickListener(v -> startActivityForResult(
+                new Intent(getApplicationContext(), CreateNoteActivity.class),
+                REQUEST_CODE_ADD_NOTE
+        ));
+    }
+
+    private void getNotes() {
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+
+            GetNotesTask() {
+                super();
+            }
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDatabase.getNoteDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+        }
+        new GetNotesTask().execute();
     }
 }
