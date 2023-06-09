9d72771f0c267a39a04b79b309f50246944cf25d
ngtienndungg <dungnt.works@gmail.com>
2023-06-10 00:30:27 +0700

    handle display notes feature

--- app/src/main/java/com/example/notes/activities/MainActivity.java
+++ app/src/main/java/com/example/notes/activities/MainActivity.java
@@ -4,15 +4,19 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.notes.R;
+import com.example.notes.adapters.NoteAdapter;
 import com.example.notes.database.NoteDatabase;
 import com.example.notes.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
 
     private ImageView ivAddNoteMain;
+    private RecyclerView rvNotes;
+
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +40,16 @@ public class MainActivity extends AppCompatActivity {
         // Handle set text event
         onClickHandling();
 
+        // Setup recycler view
+        setupRecyclerView();
+
         // Handle click event
         getNotes();
     }
 
     private void viewMapping() {
         ivAddNoteMain = findViewById(R.id.activity_main_ivAddNoteMain);
+        rvNotes = findViewById(R.id.activity_main_rvNotes);
     }
 
     private void onClickHandling() {
@@ -60,12 +72,35 @@ public class MainActivity extends AppCompatActivity {
                 return NoteDatabase.getNoteDatabase(getApplicationContext()).noteDao().getAllNotes();
             }
 
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    noteAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    noteAdapter.notifyItemInserted(0);
+                }
+                rvNotes.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
+    }
+
+    private void setupRecyclerView() {
+        rvNotes.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        noteList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(noteList);
+        rvNotes.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
     }
 }
